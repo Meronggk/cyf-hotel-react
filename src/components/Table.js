661678078b8fa3828df028cdc6f02eec2be4178d
Marryggk <99277162+Meronggk@users.noreply.github.com>
2022-12-27 15:42:21 +0000

    added table content

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,25 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
-const Table = result => {
+function Table() {
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch(`https://cyf-react.glitch.me/`)
+      .then(response => response.json())
+      .then(data => {
+        // console.log(actualData);
+        setData(data);
+        // console.log(data);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="table">
       <tbody>
@@ -13,9 +32,26 @@ const Table = result => {
           <th>Check in date</th>
           <th>Check out date</th>
         </tr>
+
+        {data.map((item, index) => (
+          <tr key={index}>
+            <td>{item.id}</td>
+            <td>{item.firstName}</td>
+            <td>{item.surname}</td>
+            <td>{item.email}</td>
+            <td>{item.roomId}</td>
+            <td>{item.checkInDate}</td>
+            <td>{item.checkOutDate}</td>
+            {/* <td>
+              <img src={item.thumbnail} alt="" height={100} />
+            </td> */}
+            {/* <td>{item.price}</td>
+            <td>{item.rating}</td> */}
+          </tr>
+        ))}
       </tbody>
     </div>
   );
-};
+}
 
 export default Table;
